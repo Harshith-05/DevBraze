237d78bf0c7b3f30c7edbe90f5c047e8bbee7b77
Harshith K <133972353+Harshith-05@users.noreply.github.com>
2024-11-23 12:23:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add a click event listener to the button
+document.getElementById("viewButton").addEventListener("click", function () {
+    // Open the link in a new tab
+    window.open("http://harshithk.me/AI-VTON/", "_blank");
+});
